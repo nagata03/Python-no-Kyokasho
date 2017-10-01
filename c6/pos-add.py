b220be15249e7b875ecd5071659bbf9d22971ac5
nagata03 <nagata.job@gmail.com>
2017-10-01 11:14:24 +0900

    update

--- c6/pos-add.py
+++ c6/pos-add.py
@@ -12,6 +12,15 @@ class Pos:
         y2 = self.y + other.y
         return Pos(x2, y2)
 
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            """変数がint型かfloat型の場合に流れるルート"""
+            x2 = self.x * other
+            y2 = self.y * other
+            return Pos(x2, y2)
+        else:
+            raise TypeError
+
     def __str__(self):
         """文字列として取得する際の振る舞いを定義"""
         return "({0}, {1})".format(self.x, self.y)
@@ -23,3 +32,7 @@ p2 = Pos(30, 40)
 # 演算子「+」を使ってみる
 p3 = p1 + p2
 print(p3)
+
+# 演算子「*」を使ってみる
+p4 = p1 * 2
+print(p4)
